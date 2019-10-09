e88c3c4e5ee3c254b9821dec794f689ee34d3d2a
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-09 12:06:34 -0400

    Created mirgration and connected to Sqlite3

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const server = express();
 
 server.use(express.json());
 
-
+server.get('/', (req,res)=>{
+    res.status(200).json({ message: "In there like swim wear"});
+});
 
 module.exports = server;
---
+++ migrations/20191009113012_add_cars_table.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.increments();
+      tbl.string('VIN', 17).notNullable();
+      tbl.string('Make', 128).notNullable();
+      tbl.string('Model', 128).notNullable();
+      tbl.string('Mileage').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
