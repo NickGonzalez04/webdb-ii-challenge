336925856d93cef7432afcc737e4fb6bf54fbfb0
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-09 12:46:06 -0400

    GET request of created data

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -24,6 +24,16 @@ router.post('/', (req,res) => {
   });
 })
 
+router.get('/', (req,res) => {
+  db('cars')
+  .first()
+    .then(cars => {
+        res.status(200).json(cars);
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'Failed to retrieve Cars'});
+    });
+});
 
 
 
